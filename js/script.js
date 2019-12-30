d5dafa726c28ce895e00bb858436622ae6829230
Verta-master <vertyagina@gmail.com>
2019-12-30 21:04:19 +0600

    Update 2019-12-30T15:04:14.928Z

--- js/script.js
+++ js/script.js
@@ -8,4 +8,8 @@ if (window.innerWidth < 1022) {
   $('.footer__title').click(function() {
     $(this).next().slideToggle();
   });
+  $('.menu__drop').click(function() {
+    $(this).next().slideToggle();
+    $(this).toggleClass('menu__drop--up');
+  });
 };
